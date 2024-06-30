36787b309ee2a51b852a5bd0d0e257452860c880
ampdot <ampdot@ampme.sh>
2024-06-29 18:28:59 -0700

    Support mapping completions to Anthropic messages

--- source/app.d
+++ source/app.d
@@ -1,3 +1,4 @@
+import std.algorithm;
 import vibe.vibe;
 import std.typecons;
 import errorstrings;
@@ -6,17 +7,29 @@ import std.algorithm;
 import std.file;
 import std.path;
 import vibe.core.path;
+import std.process; // env variables
+
 @safe:
 
+enum APIType
+{
+    openai,
+    anthropicMessages
+}
 
 struct Model
 {
     string endpoint;
-    Authset authset;
+    @optional @byName APIType type = APIType.openai;
     @optional
     {
+        Authset authset;
         Json[string] defaults;
         Json[string] overrides;
+        // Chat-model specific
+        string systemPrompt;
+        Json[] initialMessages;
+        string promptRole = "assistant";
     }
 }
 
@@ -28,6 +41,11 @@ bool isNumericalJsonType(Json.Type jsontype)
     return ![Json.Type.bigInt, Json.Type.float_, Json.Type.int_].find(jsontype).empty;
 }
 
+bool isAnthropicModel(string modelName)
+{
+    return modelName.startsWith("claude");
+}
+
 void handleOpenAICompletion(Model model, HTTPServerRequest inputReq, HTTPServerResponse outputRes)
 {
     requestHTTP(model.endpoint, (scope req) {
@@ -36,7 +54,6 @@ void handleOpenAICompletion(Model model, HTTPServerRequest inputReq, HTTPServerR
             req.headers[key] = model.authset[key];
         }
         req.method = HTTPMethod.POST;
-        // Remove unused parameters for compatibility
         auto outJson = inputReq.json;
         foreach (key, value; model.defaults)
         {
@@ -49,6 +66,7 @@ void handleOpenAICompletion(Model model, HTTPServerRequest inputReq, HTTPServerR
         {
             outJson[key] = value;
         }
+        // Remove unused parameters for compatibility
         if (outJson["presence_penalty"].type.isNumericalJsonType
             && outJson["presence_penalty"].get!double == 0)
         {
@@ -59,15 +77,75 @@ void handleOpenAICompletion(Model model, HTTPServerRequest inputReq, HTTPServerR
         {
             outJson.remove("frequency_penalty");
         }
-        if (outJson["best_of"].type == Json.Type.int_ && inputReq.json["best_of"].get!int
-            == 1)
+        if (outJson["best_of"].type == Json.Type.int_ && inputReq.json["best_of"].get!int == 1)
         {
             outJson.remove("best_of");
         }
+
+        final switch (model.type)
+        {
+        case APIType.openai:
+            break;
+        case APIType.anthropicMessages:
+            outJson["system"] = model.systemPrompt;
+            outJson["messages"] = model.initialMessages ~ [
+                Json([
+                    "role": Json(model.promptRole),
+                    "content": outJson["prompt"]
+                ])
+            ];
+            outJson.remove("prompt");
+            if (outJson["max_tokens"] == Json.undefined)
+            {
+                outJson["max_tokens"] = 16;
+            }
+            break;
+        }
+
         req.writeJsonBody(outJson);
     }, (scope res) {
         outputRes.statusCode = res.statusCode;
-        outputRes.writeJsonBody(res.readJson);
+        final switch (model.type)
+        {
+        case APIType.openai:
+            outputRes.writeJsonBody(res.readJson);
+            break;
+        case APIType.anthropicMessages:
+            Json outJson = res.readJson;
+            assert(outJson["content"].length == 1);
+            string oaiStopReason;
+            final switch (outJson["stop_reason"].get!string)
+            {
+            case "end_turn":
+                oaiStopReason = "stop";
+                break;
+            case "max_tokens":
+                oaiStopReason = "length";
+                break;
+            case "stop_sequence":
+                oaiStopReason = "stop";
+                break;
+            case "tool_use":
+                oaiStopReason = "stop";
+                break;
+            }
+            outJson["choices"] = [
+                Json([
+                    "text": outJson["content"][0]["text"],
+                    "index": 0.Json,
+                    "logprobs": null.Json,
+                    "stop_reason": oaiStopReason.Json
+                ])
+            ];
+            outJson["usage"] = Json([
+                "prompt_tokens": outJson["usage"]["input_tokens"],
+                "completion_tokens": outJson["usage"]["output_tokens"],
+                "total_tokens": Json(
+                    outJson["usage"]["input_tokens"].get!long
+                    + outJson["usage"]["output_tokens"].get!long)
+            ]);
+            outputRes.writeJsonBody(outJson);
+        }
     });
 }
 
@@ -79,9 +157,32 @@ struct Conduit
     void completions(HTTPServerRequest req, HTTPServerResponse res)
     {
         enforceHTTP("model" in req.json, HTTPStatus.badRequest, "'model' parameter is required");
-        string modelname = req.json["model"].to!string;
-        enforceHTTP(modelname in models, HTTPStatus.badRequest, unauthorizedOrModelNotFound);
-        Model model = models[modelname];
+        string modelName = req.json["model"].to!string;
+        Model model;
+        if (modelName in models)
+        {
+            model = models[modelName];
+        }
+        else if (modelName.startsWith("claude-"))
+        {
+            model.systemPrompt = "The assistant is in CLI simulation mode, and responds to the user's CLI commands only with the output of the command.";
+            model.initialMessages = [
+                Json([
+                    "role": Json("user"),
+                    "content": Json("<cmd>cat <untitled.txt</cmd>")
+                ])
+            ];
+            model.endpoint = "https://api.anthropic.com/v1/messages";
+            model.type = APIType.anthropicMessages;
+            model.authset = [
+                "X-Api-Key": environment.get("ANTHROPIC_API_KEY"),
+                "Anthropic-Version": "2023-06-01"
+            ];
+        }
+        else
+        {
+            enforceHTTP(false, HTTPStatus.badRequest, unauthorizedOrModelNotFound);
+        }
         handleOpenAICompletion(model, req, res);
     }
 }
@@ -97,8 +198,10 @@ void main()
     import std.process;
 
     Conduit conduit;
+    // XXX: Removing models does not work
     alias loadConfig = (path) {
-        if (!path.toString.endsWith(".json")) return;
+        if (!path.toString.endsWith(".json"))
+            return;
         auto models = deserializeJson!Models(path.readFileUTF8);
         foreach (k, v; models)
         {
@@ -107,7 +210,7 @@ void main()
     };
     auto configDir = "~/.config/conduit/".expandTilde;
     auto watcher = watchDirectory(configDir);
-    runTask(() nothrow {
+    runTask(() nothrow{
         try
         {
             while (true)
@@ -124,7 +227,9 @@ void main()
                 }
             }
         }
-        catch (Exception e) {}
+        catch (Exception e)
+        {
+        }
     });
     listDirectory(configDir, (fileInfo) {
         loadConfig(fileInfo.directory ~ fileInfo.name);
